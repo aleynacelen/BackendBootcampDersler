f1735ac8432bd2960f9996b83ec0f6dd707a9d91
aleynacelen <aleyna_celen@hotmail.com>
2024-10-06 15:00:59 +0300

    06/10

---
+++ c#programlama/week 10/04-JAVASCRIPT/script.js
@@ -0,0 +1,9 @@
+document.getElementById('button-external').addEventListener('mouseover', function () {
+    alert('işte üzerime geldin!')
+
+});
+
+document.getElementById('button-external').addEventListener('dblclick', function () {
+    alert('işte çift tıkladın')
+
+});
