d0db4c275688e041c2a37e9649455eaf32580947
aleynacelen <aleyna_celen@hotmail.com>
2024-10-20 14:15:54 +0300

    20/10/24

---
+++ 05-MVC/Week11/19-10-24/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _19_10_24.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ 05-MVC/Week11/19-10-24/Project07_MVCTemelleri/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project07_MVCTemelleri.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
