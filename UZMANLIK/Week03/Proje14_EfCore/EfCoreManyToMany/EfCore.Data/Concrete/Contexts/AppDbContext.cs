6ff6a7bdcfb6784bf406a4b9a3fe0cca25945807
aleynacelen <aleyna_celen@hotmail.com>
2024-12-21 23:13:35 +0300

    ..

--- UZMANLIK/Week03/Proje14_EfCore/EfCoreManyToMany/EfCore.Data/Concrete/Contexts/AppDbContext.cs
+++ UZMANLIK/Week03/Proje14_EfCore/EfCoreManyToMany/EfCore.Data/Concrete/Contexts/AppDbContext.cs
@@ -6,16 +6,14 @@ namespace EfCore.Data.Concrete.Contexts;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+    {
+        
+    }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<ProductCategory> ProductCategories { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Server=SINIF115\\SQLEXPRESS;Database=EfCoreDbMany;User=sa;Password=Qwe123.,;TrustServerCertificate=true");
-        base.OnConfiguring(optionsBuilder);
-    }
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // modelBuilder.Entity<ProductCategory>().HasNoKey();
