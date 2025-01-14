c6d08a26dbea660c86d93b2abad688973f7ea4f1
aleynacelen <aleyna_celen@hotmail.com>
2025-01-14 14:18:05 +0300

    ---

--- UZMANLIK/week06/EShop/EShop.API/Controllers/AuthsController.cs
+++ UZMANLIK/week06/EShop/EShop.API/Controllers/AuthsController.cs
@@ -16,3 +16,19 @@ namespace EShop.API.Controllers
 
 }
 //JWT
+//jhoson web token birr anahtardır 
+//herkese acık bir şekilde bilgi göndermek istemıyorum sifrelemek istiyorum login olma mekanızmasını bu sistemin üzerine kuruyoryz
+//cookie için api mantıklı değil
+// artık bana yaptığın isteklerde bana json web tokenı da gönder diyoruz
+//appseting jsonda calısıyoruz 
+// "JwtConfig":{
+//     "Secret":"şifre yazılılack guid gibi bir yerden",
+//     "Issuer":"EShopBackend", tokenı dağıtan taraf
+//     "Audience":"EshopFronted": bu tokenı kim kullanıcak
+//     "AccsessTokenExpriration":30 kullanılcak süre
+
+// }
+//progrsm cs de builder.Services.AddIdentity<ApplicationUser,ApllicationRole>(option=>{
+//option.Password.RequireDigit});
+
+
