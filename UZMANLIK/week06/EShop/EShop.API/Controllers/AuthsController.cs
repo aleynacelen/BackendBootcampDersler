3cbc78232ef12a711b7e8b2a4a80be93e8021f26
aleynacelen <aleyna_celen@hotmail.com>
2025-01-15 10:50:37 +0300

    15-01

--- UZMANLIK/week06/EShop/EShop.API/Controllers/AuthsController.cs
+++ UZMANLIK/week06/EShop/EShop.API/Controllers/AuthsController.cs
@@ -23,13 +23,26 @@ namespace EShop.API.Controllers
 //appseting jsonda calısıyoruz 
 // "JwtConfig":{
 //     "Secret":"şifre yazılılack guid gibi bir yerden",
-//     "Issuer":"EShopBackend", tokenı dağıtan taraf
+//     "Issuer":"EShopBackend", t okenı dağıtan taraf
 //     "Audience":"EshopFronted": bu tokenı kim kullanıcak
 //     "AccsessTokenExpriration":30 kullanılcak süre
 
 // }
 //progrsm cs de builder.Services.AddIdentity<ApplicationUser,ApllicationRole>(option=>{
-//option.Password.RequireDigit});
+//option.Password.RequireDigit}); şifre ile ilgili ayarları yaptık
+//AddEntityFrameworStores = add ıdentıtynın deposu neresesi diyor
+//AddEntityFrameworkStore<EShopDbContex 
+//AddDefaultTokenProviders(); => Bunu yükleme sebebimiz ise token gönderebilmemeizi ssağlar eğer bunu yüksemezsek token üretmek için ayrıca bır paket yüklememiz gerekir
+//shared appseting json dakı bilgilerin bulundugu bir class oluşturacağız
+//sharedde configutastion adında bir folder oluşturduk ve sjwtconfig adında bir class oluşturduk ve çine secret vb proplar ekledik
+//program cse tekrar geçtik ve oraya 
+//builder.Services.Configure<JwConfig>(builder.Configaration.GetSection("JwtConfig")); i oluşturdum
+//Bu kod bana configasryon ayayı yaptım ve appsettinge eriştim 
+//var jwtConfig= builder.Configuration.GetSection("JwtConfig").Get<JwtConfig>(); //bu kodu yazdık cunku baska bır klasın ıcınde
+// builder.Services.AddAuthentication (options=>{option.s})
+//nuget.orga gidiyoruz ve jwtbearres paketini yüklüyouruz
+///
+
 
 
 
