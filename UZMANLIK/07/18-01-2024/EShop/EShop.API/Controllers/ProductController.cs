8c029280b5f5b64dfb0ba8fe1828cf9b2635a358
aleynacelen <aleyna_celen@hotmail.com>
2025-01-27 19:30:42 +0300

    2400

---
+++ UZMANLIK/07/18-01-2024/EShop/EShop.API/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;   
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+    }
+}
