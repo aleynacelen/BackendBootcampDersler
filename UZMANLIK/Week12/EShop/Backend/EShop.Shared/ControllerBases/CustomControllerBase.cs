2373e0761d854c6f15c3bc2549a7064349b7d6b7
aleynacelen <aleyna_celen@hotmail.com>
2025-03-07 23:34:34 +0300

    --

--- UZMANLIK/Week12/EShop/Backend/EShop.Shared/ControllerBases/CustomControllerBase.cs
+++ UZMANLIK/Week12/EShop/Backend/EShop.Shared/ControllerBases/CustomControllerBase.cs
@@ -1,4 +1,5 @@
 using EShop.Shared.Dtos.ResponseDtos;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,6 +19,21 @@ namespace EShop.Shared.ControllerBases
         {
             var userId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
             return userId!;
+
         }
     }
+
 }
+
+public class CustomControllerBase: ControllerBase
+{
+    public static IActionResult CreateResult<T>(ResponseDto<T> response)
+    {
+        return new ObjectResult(response)
+        {
+            StatusCode = response.StatusCode
+        };
+    }
+}
+//Bu kodda, ControllerBase sınıfından türetilen CustomControllerBase sınıfı oluşturulmuştur. Bu sınıf, API'lerde kullanılan controller sınıflarının ortak işlemlerini gerçekleştirmek için oluşturulmuştur. CreateResult metodu, API'lerden dönen ResponseDto nesnelerini IActionResult nesnelerine dönüştürmek için kullanılmaktadır. GetUserId metodu, kullanıcının kimliğini döndürmek için kullanılmaktadır.
+//Bu sınıf, API'lerde kullanılan controller sınıflarının ortak işlemlerini gerçekleştirmek için oluşturulmuştur. CreateResult metodu, API'lerden dönen ResponseDto nesnelerini IActionResult nesnelerine dönüştürmek için kullanılmaktadır. GetUserId metodu, kullanıcının kimliğini döndürmek için kullanılmaktadır.
--- UZMANLIK/Week12/EShop/Frontend/EShop.MVC/Areas/Admin/Controllers/ProductController.cs
+++ UZMANLIK/Week12/EShop/Frontend/EShop.MVC/Areas/Admin/Controllers/ProductController.cs
@@ -1,11 +1,22 @@
 using EShop.MVC.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
 
 namespace EShop.MVC.Areas.Admin.Controllers
 {
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly  IToastNotification _toastNotification;
+
+        public ProductController(IProductService productService, ICategoryService categoryService, IToastNotification toastNotification)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+            _toastNotification = toastNotification;
+        }
+
         public ActionResult Index()
         {
             return View();
