66e4ff15c182fc2f8ddc8eb730474eb0977038da
aleynacelen <aleyna_celen@hotmail.com>
2025-04-12 19:45:34 +0300

    --

---
+++ UZMANLIK/Kendi Çalışmalarım/AleyBlog/Data/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+using System;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+
+public class AppDbContext : DbContext
+{
+    protected AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+    {
+    }
+    public DbSet<Post>  Posts { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
