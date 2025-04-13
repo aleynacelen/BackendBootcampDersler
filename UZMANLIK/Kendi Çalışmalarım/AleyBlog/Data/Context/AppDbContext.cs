e7eb651884802d5b91183d359981444d603317c9
aleynacelen <aleyna_celen@hotmail.com>
2025-04-13 10:26:16 +0300

    .

--- UZMANLIK/Kendi Çalışmalarım/AleyBlog/Data/Context/AppDbContext.cs
+++ UZMANLIK/Kendi Çalışmalarım/AleyBlog/Data/Context/AppDbContext.cs
@@ -12,4 +12,5 @@ public class AppDbContext : DbContext
     public DbSet<Post>  Posts { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<U
 }
