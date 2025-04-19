cdd06fb6dea2f7f96b1fe84ae7cf204859055516
aleynacelen <aleyna_celen@hotmail.com>
2025-04-19 16:15:39 +0300

    --

--- UZMANLIK/Kendi Çalışmalarım/AleyBlog/Data/Concrate/PostRepository.cs
+++ UZMANLIK/Kendi Çalışmalarım/AleyBlog/Data/Concrate/PostRepository.cs
@@ -1,8 +1,41 @@
 using System;
+using Data.Abstract;
+using Data.Context;
+using Entities;
 
 namespace Data.Concrate;
 
-public class PostRepository
+public class PostRepository : IPost
 {
+    private readonly AppDbContext _context;
 
+    public PostRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task AddAsync(Post entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Post entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Post>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Post entity)
+    {
+        throw new NotImplementedException();
+    }
 }
--- UZMANLIK/Kendi Çalışmalarım/AleyBlog/Data/Concrate/UserRepository.cs
+++ UZMANLIK/Kendi Çalışmalarım/AleyBlog/Data/Concrate/UserRepository.cs
@@ -1,8 +1,41 @@
 using System;
+using Data.Abstract;
+using Data.Context;
+using Entities;
 
 namespace Data.Concrate;
 
-public class UserRepository
+public class UserRepository : IUser
 {
+    private readonly AppDbContext _contex;
 
+    public UserRepository(AppDbContext contex)
+    {
+        _contex = contex;
+    }
+
+    public Task AddAsync(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<User>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(User entity)
+    {
+        throw new NotImplementedException();
+    }
 }
